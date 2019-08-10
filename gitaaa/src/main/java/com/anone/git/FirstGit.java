0653e65b594c26c17a6cda181c659fa2ddbd799b
anone <941199658@qq.com>
2019-08-10 15:51:56 +0800

    SecondCommit

---
+++ gitaaa/src/main/java/com/anone/git/FirstGit.java
@@ -0,0 +1,10 @@
+package com.anone.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        /**
+         * 无聊的对白
+         */
+        System.out.println("圣诞结 你听过吗！！");
+    }
+}
